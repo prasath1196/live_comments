9ea001059834b613d251d3fecbc7dd1d877ece7f
prasath <prasath1196@gmail.com>
2019-05-01 13:34:44 +0530

    Post Creation Completed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+
 //= require turbolinks
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require jquery_ujs
 //= require_tree .
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,26 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @posts = current_user.posts.all
   end
 
   def show
   end
 
   def create
+    if current_user.posts.create!(post_params)
+      redirect_to root_path
+    else
+      redirect_to :new_post_path
+    end
   end
 
   def new
+    @post = current_user.posts.new
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:content)
   end
 end
